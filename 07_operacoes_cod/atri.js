aa734eda3a1c009507ac8fd8400aa284422653ee
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2026-01-19 15:17:48 -0300

    add atri.js

---
+++ 07_operacoes_cod/atri.js
@@ -0,0 +1,15 @@
+let preco = 10.00
+
+console.log(`Preço original = ${preco}`)
+
+preco += 5
+
+console.log(`Preço somado a 5 = ${preco}`)
+
+preco /= 5
+
+console.log(`Preço dividido por 5 = ${preco}`)
+
+preco %= 2
+
+console.log(`O resto de preço, dividido por 2 = ${preco}`)
