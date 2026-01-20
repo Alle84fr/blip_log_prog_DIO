d4cf59ce26a1a6b58e14900b32ac9345c22c5eda
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2026-01-20 14:20:07 -0300

    add ope_comp_2.js

---
+++ 07_operacoes_cod/ope_comp_2.js
@@ -0,0 +1,21 @@
+let idade1 = 78
+let idade2 = 18
+let idade3 = 17
+
+let maior_dade = 18
+
+console.log(`User 1 é maior de idade? ` +(idade1>=maior_dade))
+console.log(`User 2 é maior de idade? ` +(idade2>=maior_dade))
+console.log(`User 2 é tem mais de 18 anos? ` +(idade2>maior_dade))
+console.log(`User 3 é maior de idade? ` +(idade3>=maior_dade))
+console.log(`User 3 mais novo que user 1? ` +(idade3<idade1))
+console.log(`User 3 tem 17 anos? ` +(idade3==17))
+
+let minus = "minuscula"
+let maius = "Minuscula"
+
+console.log(` `)
+console.log(`Maioria da linguagen são case sensitive, maiusculas e minusculas são diferentes
+    ${minus} == ${maius}: ` + (minus==maius) )
+console.log(` `)
+
